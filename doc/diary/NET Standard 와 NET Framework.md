1c607033942f084da1a7aadad33f27a74c6c09a4
nemonuri <nemonuri2@gmail.com>
2026-01-17 16:26:35 +0900

    휴, 빌드 성공!

---
+++ doc/diary/NET Standard 와 NET Framework.md
@@ -0,0 +1,10 @@
+# NET Standard 와 NET Framework
+
+- NET Standard 2.0 은, NET Framework 4.6.1 부터 지원한다!
+  - NET Standard 2.0 라이브러리가 NET Framework 4.6 을 대상 프레임워크로 빌드했으니, 당연히 문제가 생겼지...
+  - .NET 공식 문서에서는, NET Framework 4.7.2 이상을 사용할 것을 권장한다.
+
+## 참고자료
+
+- https://learn.microsoft.com/en-us/dotnet/standard/library-guidance/cross-platform-targeting
+- https://learn.microsoft.com/en-us/dotnet/standard/net-standard?tabs=net-standard-2-0
--- src/Nemonuri.RepoTools.FStar.BuildTasks/GeneratePrivateFStarConfigJson.fs
+++ src/Nemonuri.RepoTools.FStar.BuildTasks/GeneratePrivateFStarConfigJson.fs
@@ -1,5 +1,25 @@
 ﻿namespace Nemonuri.RepoTools.FStar.BuildTasks
+open Microsoft.Build.Framework
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type public GeneratePrivateFStarConfigJson() as this =
+    inherit Microsoft.Build.Utilities.Task()
+
+    [<Required>]
+    member val FStarExe: string = "" with get,set
+
+    member val Options: ITaskItem[] = [||] with get,set
+
+    member val IncludeDirs: ITaskItem[] = [||] with get,set
+
+    [<Required>]
+    member val OutDirectoryPath: string = "" with get,set
+
+    member val Prefix: string = "" with get,set
+
+    override _.Execute(): bool =
+        try
+            true
+        with e ->
+            this.Log.LogErrorFromException(e)
+            false
+
