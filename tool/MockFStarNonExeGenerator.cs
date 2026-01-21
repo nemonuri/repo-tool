d003d165ca6b8f94f93b5db8be0779008f75c361
nemonuri <nemonuri2@gmail.com>
2026-01-21 11:46:34 +0900

    WorkingDirectory 를 ToolRootPath 와 똑같게 설정해야, 기대했던 효과를 볼 수 있구나...

--- tool/MockFStarNonExeGenerator.cs
+++ tool/MockFStarNonExeGenerator.cs
@@ -1,11 +1,30 @@
 #!/usr/bin/env dotnet
 
-foreach (var a in args)
-{
-    Console.WriteLine(a);
+if (args.Length < 1 || string.IsNullOrWhiteSpace(args[0])) 
+{ 
+    Console.Error.WriteLine("First argument should be directory path.");
+    return 1;
 }
 
-if (args.Length != 1) 
-{ 
-    //Console.Error.WriteLine(string.Join(',')) 
+var dirFullPath = Path.GetFullPath(args[0]);
+if (!Directory.Exists(dirFullPath)) { Directory.CreateDirectory(dirFullPath); }
+
+
+List<string> toStdOuts = new();
+
+// directory.fstar.exe
+toStdOuts.Add(Directory.CreateDirectory(Path.Combine(dirFullPath, "directory.fstar.exe")).FullName );
+
+// fstar.txt
+{
+    string txtPath = Path.Combine(dirFullPath, "fstar.txt");
+    File.WriteAllText(txtPath, "");
+    toStdOuts.Add(txtPath);
 }
+
+foreach (var item in toStdOuts)
+{
+    Console.WriteLine(item);
+}
+
+return 0;
