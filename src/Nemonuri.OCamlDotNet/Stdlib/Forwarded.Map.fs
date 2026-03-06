d8280b9c7e1ba8b3d5906309042a7ba452597c33
nemonuri <nemonuri2@gmail.com>
2026-03-06 20:24:36 +0900

    별도의 'Functor' 는....필요하지 않았다;;

--- src/Nemonuri.OCamlDotNet/Stdlib/Forwarded.Map.fs
+++ src/Nemonuri.OCamlDotNet/Stdlib/Forwarded.Map.fs
@@ -1,5 +1,19 @@
 namespace Nemonuri.OCamlDotNet.Forwarded
 
-module Map = 
+module Fm = Microsoft.FSharp.Collections.Map
 
-    type S<'key>
+/// https://ocaml.org/manual/5.4/api/Map.Make.html
+module Map =   
+
+    [<AbstractClass; Sealed>]
+    type S<'key when 'key : comparison> =
+
+        static member empty<'a>() : t<'key,'a> = Fm.empty<'key,'a>
+
+        static member add<'a> key data m : t<'key,'a> = Fm.add key data m
+
+        static member of_list<'a> bs : t<'key,'a> = Fm.ofList bs
+
+        static member find_opt<'a> (x: 'key) (m: t<'key,'a>)  = Fm.tryFind x m
+
+    and t<'key,'a when 'key : comparison> = Microsoft.FSharp.Collections.Map<'key,'a>
